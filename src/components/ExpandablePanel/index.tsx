239c7de0bc19969ebf211a09028e9e14eb8d0e0b
Ruggero Visintin <RuggeroVisintin@users.noreply.github.com>
2025-11-09 16:58:09 +0100

    feat(entity-props): group components into expandable panels (#252)
    * feat: add expandable panels for entity components
    
    * feat: add visual clue for panel state
    
    ---------
    
    Co-authored-by: Ruggero Visintin <9091842+RuggeroVisintin@users.noreply.github.com>

---
+++ src/components/ExpandablePanel/index.tsx
@@ -0,0 +1,38 @@
+import React from "react"
+import { BackgroundColor, Box, BoxProps, FlexBox, Spacing } from "../../primitives"
+
+interface ExpandablePanelProps extends BoxProps {
+    children?: React.ReactNode;
+    title?: string;
+}
+
+export const ExpandablePanel = ({ children, title, ...boxProps }: ExpandablePanelProps) => {
+    const [isExpanded, setIsExpanded] = React.useState(false);
+
+    return (
+        <Box {...boxProps}>
+            <Box
+                data-testid="ExpandablePanel.Title" onClick={() => setIsExpanded(!isExpanded)}
+                $background={BackgroundColor.Secondary}
+                $spacing={Spacing.xs}
+                $hSpacing={Spacing.sm}
+                $clickable
+            >
+                <FlexBox $direction="row">
+                    <FlexBox $fill>{title}</FlexBox>
+                    <FlexBox>{isExpanded ? '-' : '+'}</FlexBox>
+                </FlexBox>
+
+            </Box>
+            {isExpanded &&
+                <Box
+                    $spacing={Spacing.xs}
+                    $hSpacing={Spacing.sm}
+                >
+                    {children}
+                </Box>
+            }
+
+        </Box>
+    )
+}
