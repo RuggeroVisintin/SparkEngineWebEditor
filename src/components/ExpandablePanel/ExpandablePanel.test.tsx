309121ec152485607078512d843f517b61b1de4d
Ruggero Visintin <RuggeroVisintin@users.noreply.github.com>
2025-11-09 16:58:09 +0100

    feat(entity-props): group components into expandable panels (#252)
    * feat: add expandable panels for entity components
    
    * feat: add visual clue for panel state
    
    ---------
    
    Co-authored-by: Ruggero Visintin <9091842+RuggeroVisintin@users.noreply.github.com>

---
+++ src/components/ExpandablePanel/ExpandablePanel.test.tsx
@@ -0,0 +1,58 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { ExpandablePanel } from ".";
+import React from "react";
+
+describe('ExpandablePanel', () => {
+    it('Should be collapsed by default', () => {
+        render(
+            <ExpandablePanel>
+                <div>This is a child element</div>
+                <p>Another child element</p>
+            </ExpandablePanel>
+        );
+        const content = screen.queryByText('This is a child element');
+        expect(content).not.toBeInTheDocument();
+    });
+
+    it('Should expand when clicked from collapsed', () => {
+        render(
+            <ExpandablePanel>
+                <div>This is a child element</div>
+                <p>Another child element</p>
+            </ExpandablePanel>
+        );
+
+        const panel = screen.getByTestId('ExpandablePanel.Title');
+        fireEvent.click(panel);
+
+        const content = screen.queryByText('This is a child element');
+        expect(content).toBeInTheDocument();
+    });
+
+    it('Should collapse when clicked from expansed', () => {
+        render(
+            <ExpandablePanel>
+                <div>This is a child element</div>
+                <p>Another child element</p>
+            </ExpandablePanel>
+        );
+
+        const panel = screen.getByTestId('ExpandablePanel.Title');
+        fireEvent.click(panel);
+        fireEvent.click(panel);
+
+        const content = screen.queryByText('This is a child element');
+        expect(content).not.toBeInTheDocument();
+
+    });
+
+    it('Should display a given title', () => {
+        render(
+            <ExpandablePanel title="My Expandable Panel">
+            </ExpandablePanel>
+        );
+
+        const title = screen.queryByText('My Expandable Panel');
+        expect(title).toBeVisible();
+    })
+});
