34b40c20d1d436aa34f1af6d90fe693ff1958470
Ruggero Visintin <9091842+RuggeroVisintin@users.noreply.github.com>
2025-12-26 13:02:42 +0100

    refactor: tidy

--- src/templates/EntityPropsPanel/components/DynamicPropsGroup/index.test.tsx
+++ src/templates/EntityPropsPanel/components/DynamicPropsGroup/index.test.tsx
@@ -5,81 +5,70 @@ import React from "react";
 
 describe('EntityPropsPanel/components/DynamicPropsGroup', () => {
 
-    it('Should render number component props', () => {
-        const mockComponent = {
-            toJson: () => ({
-                __type: 'MockComponent',
-                numericProp: 100
-            })
-        } as unknown as IComponent;
+    describe('Primitives', () => {
+        it.each([
+            ['number', 'spinbutton', 100],
+            ['text', 'textbox', 'TestString'],
+        ])('Should render %s component props', (_, ariaRole, value) => {
+            const mockComponent = {
+                toJson: () => ({
+                    __type: 'MockComponent',
+                    testProp: value
+                })
+            } as unknown as IComponent;
 
-        const view = render(<DynamicPropsGroup component={mockComponent} />);
+            const view = render(<DynamicPropsGroup component={mockComponent} />);
 
-        const numericPropGroup = view.getByRole('group', { name: 'Numeric Prop' });
+            const testPropGroup = view.getByRole('group', { name: 'Test Prop' });
 
-        expect(numericPropGroup).toBeVisible();
-        expect(within(numericPropGroup).getByRole('spinbutton', { name: '' })).toHaveValue(100);
+            expect(testPropGroup).toBeVisible();
+            expect(within(testPropGroup).getByRole(ariaRole, { name: '' })).toHaveValue(value);
+        });
+
+        it('Should map properties names to capitalized labels', async () => {
+            const mockComponent = {
+                toJson: () => ({
+                    __type: 'MockComponent',
+                    diffuseColor: '#FF0000',
+                    opacity: 0.5
+                })
+            } as unknown as IComponent;
+
+            const view = render(<DynamicPropsGroup component={mockComponent} />);
+
+            const diffuseColorGroup = await view.findByRole('group', { name: 'Diffuse Color' });
+            expect(diffuseColorGroup).toBeVisible();
+
+            const opacityGroup = await view.findByRole('group', { name: 'Opacity' });
+            expect(opacityGroup).toBeVisible();
+        });
+
+        it.each([
+            ['number', 'spinbutton', 0, 20],
+            ['text', 'textbox', 'TestString', 'NewString'],
+        ])('Should invoke onChange callbacks when properties are changed for %s', async (_, ariaRole, value, newValue) => {
+            const onChangeMock = jest.fn();
+
+            const mockComponent = {
+                toJson: () => ({
+                    __type: 'MockComponent',
+                    prop: value
+                })
+            } as unknown as IComponent;
+
+            const view = render(<DynamicPropsGroup component={mockComponent} onChange={onChangeMock} />);
+
+            const propGroup = view.getByRole('group', { name: 'Prop' });
+            const propInput = within(propGroup).getByRole(ariaRole, { name: '' });
+
+            fireEvent.change(propInput, { target: { value: newValue } });
+
+            expect(onChangeMock).toHaveBeenCalledWith('prop', newValue);
+        });
     });
 
-    it('Should render string component props', () => {
-        const mockComponent = {
-            toJson: () => ({
-                __type: 'MockComponent',
-                stringProp: 'TestString'
-            })
-        } as unknown as IComponent;
-
-        const result = render(<DynamicPropsGroup component={mockComponent} />);
-
-        const stringPropGroup = result.getByRole('group', { name: 'String Prop' });
-
-        expect(stringPropGroup).toBeVisible();
-        expect(within(stringPropGroup).getByRole('textbox', { name: '' })).toHaveValue('TestString');
-    });
-
-    it('Should map properties names to capitalized labels', async () => {
-        const mockComponent = {
-            toJson: () => ({
-                __type: 'MockComponent',
-                diffuseColor: '#FF0000',
-                opacity: 0.5
-            })
-        } as unknown as IComponent;
-
-        const view = render(<DynamicPropsGroup component={mockComponent} />);
-
-        const diffuseColorGroup = await view.findByRole('group', { name: 'Diffuse Color' });
-        expect(diffuseColorGroup).toBeVisible();
-
-        const opacityGroup = await view.findByRole('group', { name: 'Opacity' });
-        expect(opacityGroup).toBeVisible();
-    });
-
-    it.each([
-        ['spinbutton', 0, 20],
-        ['textbox', 'TestString', 'NewString'],
-    ])('Should invoke onChange callbacks when properties are changed with value %s', async (ariaRole, value, newValue) => {
-        const onChangeMock = jest.fn();
-
-        const mockComponent = {
-            toJson: () => ({
-                __type: 'MockComponent',
-                prop: value
-            })
-        } as unknown as IComponent;
-
-        const view = render(<DynamicPropsGroup component={mockComponent} onChange={onChangeMock} />);
-
-        const propGroup = view.getByRole('group', { name: 'Prop' });
-        const propInput = within(propGroup).getByRole(ariaRole, { name: '' });
-
-        fireEvent.change(propInput, { target: { value: newValue } });
-
-        expect(onChangeMock).toHaveBeenCalledWith('prop', newValue);
-    });
-
-    describe('Nested Properties', () => {
-        it('Should trim nested component properties label by keeping the first letter capitalized', () => {
+    describe('Complex Objects', () => {
+        it('Should trim complex object\'s properties label by keeping the first letter capitalized', () => {
             const mockComponent = {
                 toJson: () => ({
                     __type: 'MockComponent',
@@ -129,7 +118,7 @@ describe('EntityPropsPanel/components/DynamicPropsGroup', () => {
             expect(innerNumberInput).toHaveValue(42);
         });
 
-        it('Should invoke onChange callbacks when nested properties are changed', async () => {
+        it('Should invoke onChange callbacks when complex objects properties are changed', async () => {
             const onChangeMock = jest.fn();
 
             const resultJson = {
