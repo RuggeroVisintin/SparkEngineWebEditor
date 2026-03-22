fbb549829ff3edfaffb5f3301b1322c5385bb1e9
Ruggero Visintin <RuggeroVisintin@users.noreply.github.com>
2026-03-22 20:39:30 +0100

    Delete e2e/stringUtils.ts

--- e2e/stringUtils.ts
+++
@@ -1,6 +0,0 @@
-export const normalizeComparableString = (value?: string | null): string => {
-    return (value ?? '')
-        .replace(/\u00A0/g, ' ')
-        .replace(/\s+/g, ' ')
-        .trim();
-};
