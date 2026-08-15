9b4fdfda9728a671393b394cb19563b464fe78d7
Ruggero Visintin <9091842+RuggeroVisintin@users.noreply.github.com>
2026-08-15 18:47:50 +0200

    fix(preview): use stringified scene representation to avoid passing functions in the EventBus

--- src/core/preview/application/commands/PreviewScene.ts
+++ src/core/preview/application/commands/PreviewScene.ts
@@ -2,6 +2,6 @@ import { SceneJsonProps } from "@sparkengine";
 import { SerializedImageAssetSnapshot } from "../../../assets";
 
 export interface PreviewSceneCommand {
-    scene: SceneJsonProps;
+    scene: string;
     assets: SerializedImageAssetSnapshot;
 }
