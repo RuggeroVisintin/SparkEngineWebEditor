81e5dea8eeb670e3af48ee62402e0e63de5d824b
Yunus Emre Umar <77045015+emre155@users.noreply.github.com>
2026-08-16 15:09:46 +0300

    chore(ci): enforce predefined scopes in commitlint configuration
    Closes #96

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,26 @@
 module.exports = {
-    extends: ['@commitlint/config-conventional']
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+        'scope-enum': [
+            2,
+            'always',
+            [
+                'components',
+                'core',
+                'hooks',
+                'pages',
+                'primitives',
+                'providers',
+                'templates',
+                'config',
+                'types',
+                'docs',
+                'e2e',
+                'ci',
+                'deps',
+                'release',
+                'scripts'
+            ]
+        ]
+    }
 };
